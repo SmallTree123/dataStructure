44e46b965b67349cdd612abfd2f8a42d25e65b8e
SmallTree123 <1793401593@qq.com>
2020-02-10 16:03:23 +0800

    插值查找

---
+++ src/Search/插值查找.java
@@ -0,0 +1,32 @@
+package Search;
+
+import sun.net.idn.Punycode;
+
+public class 插值查找 {
+    public static void main(String[] args) {
+        int max=10000;
+        int arr[]=new int[max];
+        for (int i=0;i<max;i++){
+            arr[i]=i+1;
+        }
+
+        int valueSearch = insertValueSearch(arr, 0, arr.length - 1, max);
+        System.out.println("索引是："+valueSearch);
+    }
+    public static int insertValueSearch(int arr[],int left,int right,int findVal){
+        if (left>right ||findVal<arr[0]||findVal>arr[arr.length-1]){
+            return -1;
+        }
+        //取出mid
+        int mid=left+(right-left)*(findVal-arr[left])/(arr[right]-arr[left]);
+        int midVal=arr[mid];
+        if (findVal>midVal){
+            return insertValueSearch(arr,mid+1,right,findVal);
+        }else if (findVal<midVal){
+            return insertValueSearch(arr,left,mid-1,findVal);
+        }else {
+            return mid;
+        }
+
+    }
+}
